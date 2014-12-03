b2383d604ef429b938a952ae63f20762058d5ae0
myworks <hello@athimannil.com>
2014-12-03 12:34:01 +0000

    angularjs

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <script src="js/jquery-1.10.2.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/bootstrap-select.js"></script>
+<script src="js/angular.min.js"></script>
 <script src="js/core.js"></script>
+<script src="js/angularscript.js"></script>
 </body>
 </body>
 </html>
