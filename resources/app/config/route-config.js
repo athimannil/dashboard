d1bd1e4d00e352a9db47ff2092902d7396f261a0
Muhammed athimanil <hello@athimannil.com>
2017-05-13 23:42:01 +0200

    initial router

---
+++ resources/app/config/route-config.js
@@ -0,0 +1,48 @@
+angular
+  .module('app')
+  .config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('dashboard', {
+      url: '/dashboard',
+      templateUrl: 'public/dashboard.html',
+      controller: 'dashboardController'
+    })
+    .state('expenses', {
+      url: '/expenses',
+      templateUrl: 'public/expenses.html',
+      controller: 'expensesController',
+      controllerAs: "expensectrl"
+    })
+    .state('invoice', {
+      url: '/invoice',
+      templateUrl: 'public/invoice.html',
+      controller: 'invoiceController',
+      controllerAs: 'invoicectrl'
+    })
+    .state('msg', {
+        url: '/messageboard',
+        templateUrl: 'public/messageboard.html',
+        controller: 'messageboardController',
+        controllerAs: 'msgctrl'
+    })
+    .state('reccurr', {
+        url: '/reccurrbill',
+        templateUrl: 'public/reccurrbill.html',
+        controller: 'reccurrbillController',
+        controllerAs: 'reccurrctrl'
+    })
+    .state('profile', {
+        url: '/profile',
+        templateUrl: 'public/profile.html',
+        controller: 'profileController',
+        controllerAs: 'profilectrl'
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'public/settings.html',
+      controller: 'settingsController'
+    })
+    $urlRouterProvider.otherwise('/dashboard');
+}
